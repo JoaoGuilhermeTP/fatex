104da17adb88e660489809b52834e9ecdd783bcf
João Guilherme <adm.joaoguilherme@gmail.com>
2024-09-25 19:19:51 -0300

    Versão inicial

---
+++ app/forms.py
@@ -0,0 +1,65 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+
+# Create classes that will represent forms for our HTML files
+# We crete the classes, and they inherit from the FlaskFrom class, thats why we pass it in the paranthesis
+
+
+# Class for the registration form
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+    
+    
+# Class for the login form
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
+    
+# Class for the update profile form
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators = [FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+    
+    def validate_username(self, username):
+        # If the user changes their username, query the database to see if the new username is already taken
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+    def validate_email(self, email):
+        # If the user changes their email, query the database to see if the new email is already taken
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')   
+            
+            
+class PostForm(FlaskForm):
+    title = StringField('Title', validators = [DataRequired()])
+    content = TextAreaField('Content', validators = [DataRequired()])
+    submit = SubmitField('Post')
---
+++ run.py
@@ -0,0 +1,5 @@
+# Import the app object from the app package
+from app import app
+
+if __name__  == '__main__':
+    app.run(debug=True)
